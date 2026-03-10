75019fa07bb73dfdc91b82ccaa0503f426a69e62
Haven Johansen <havey.johansen@gmail.com>
2026-03-10 12:50:32 -0600

    debugging WIP for elevation and distance dataframes

--- gis-redo.R
+++ gis-redo.R
@@ -1,7 +1,6 @@
-#### HEADER ####
 # haven johansen
 # spring 2026
-# attempting hispaniola healthcare site/elevation/coastline geospatial analysis
+# hispaniola healthcare site/elevation/coastline geospatial analysis
 
 #### WORKSPACE ####
 
@@ -9,7 +8,8 @@ rm(list=ls())
 graphics.off()
 
 library(terra)
-library(geodata)
+
+here::here("/Users/havenjohansen/Documents/github/hospitals-hti-dom")
 
 # load country shapefiles
 hti = geodata::gadm(country = "HTI", level = 0, path = ".") 
@@ -26,25 +26,25 @@ sites_dom = vect("Dominican Republic") # nrow 1081
 #### DATA CLEANING ####
 
 # subset to remove places of worship, dentist, NA
-sites_hti = subset(sites_hti, sites_hti$amenity %in% c("hospital", "pharmacy", "clinic", "doctors")) # nrow 1988
-# confirmed successful 
+sites_hti = terra::subset(sites_hti, sites_hti$amenity == c("hospital", "pharmacy", "clinic", "doctors")) # nrow 1988
+subset(v, v$NAME_1 == "Diekirch", c("NAME_1", "NAME_2"))
+# s4 warning need to check
 
 # subset to remove dentist, NA
 sites_dom = subset(sites_dom, sites_dom$amenity %in% c("hospital", "pharmacy", "clinic", "doctors")) # nrow 945
 # confirmed successful
 
-#### COORDINATE SYSTEM ####
+# combine sites
+sites_dom_hti = union(sites_hti, sites_dom)
+# note to check that the data cleaning turned out the same in arcgis
 
-crs(hti)
-# WGS84
-crs(dom)
-# WGS84
-crs(elevation)
-# WGS84
-crs(sites_hti)
-# WGS84
-crs(sites_dom)
-# WGS84
+#### COORDINATE SYSTEM (un-comment if you want to re-check) ####
+
+# crs(hti)
+# crs(dom)
+# crs(elevation)
+# crs(sites_dom_hti)
+# all WGS 84
 
 #### POINTS TO POLYGONS ####
 
@@ -59,41 +59,44 @@ dom_hti_line = as.lines(dom_hti)
 
 #### MAKE POINTS TALK TO EACH OTHER #### 
 
-# create dataframe, extract and then attach to points layer as an attribute
-hti_df <- terra::extract(
-  x = elevation,
-  y = sites_hti,
-  na.rm = TRUE
-)
-# one column point IDs, one column associated values extracted from raster
+# create generic ID, create dataframe, extract elevation values for points and attach to attribute
+sites_dom_hti$id_check = 1:nrow(sites_dom_hti)
 
-# repeat for dom
-dom_df = terra::extract(
+df_dom_hti = terra::extract(
   x = elevation,
-  y = sites_dom,
-  na.rm = TRUE
+  y = sites_dom_hti,
+  na.rm = TRUE,
 )
 
-# add dataframe to attribute table
-elevation_merge_hti = merge(sites_hti, hti_df, by.x=c('NAME_1', 'NAME_2'), by.y=c('District', 'Canton'))
+sites_dom_hti$elevation = df_dom_hti[, 2]
+# plot-wise this looks wrong. also there are some repeat entries
 
-# 
-distance_test = distance(dom_hti_line, sites_dom)
-# no
+#### DISTANCE TO COASTLINE ####
 
-coord_dom = crds(sites_dom)
-
-#
-geosphere::dist2Line(coord_dom, dom_hti_line)
-
-
-# create blank raster and call distance into raster
+# create blank raster and call line into raster
 dom_hti_rast = rast(ext = ext(dom_hti_line), res = 0.009, crs = crs(dom_hti_line))
 line_rast = rasterize(dom_hti_line, dom_hti_rast)
 # calculate distance
-dom_hti_dist = distance(line_rast)
+dom_hti_dist = terra::distance(line_rast)
 # mask to original polygon
 dom_hti_dist = mask(dom_hti_dist, dom_hti)
+# masking back to polygon instead of line here -- returns a blank plot if you mask to line
 
 plot(dom_hti_dist)
-plot(dom_hti, add = TRUE)
+plot(dom_hti_line, add = TRUE)
+
+# create generic ID, create dataframe, extract distance values for points and attach to attribute
+dom_hti_line$id_check = 1:nrow(dom_hti_line)
+
+dist_df = terra::extract(
+  x = dom_hti_dist,
+  y = sites_dom_hti,
+  na.rm = TRUE,
+)
+
+dom_hti_line$distance = dist_df[, 2]
+
+plot(dist_df, col = map.pal("elevation", 3))
+# units are meters. concerned that there are 0s
+
+
