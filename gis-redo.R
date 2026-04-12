dbbcc181082b739a9a8e64cc7f05a0de0073f41f
Haven Johansen <havey.johansen@gmail.com>
2026-04-12 16:38:56 -0400

    DONE

--- gis-redo.R
+++ gis-redo.R
@@ -8,8 +8,8 @@ rm(list=ls())
 graphics.off()
 
 library(terra)
-# library(raster)
 library(sf)
+# library(raster)
 
 # load country shapefiles
 hti = geodata::gadm(country = "HTI", level = 0, path = ".") 
@@ -70,30 +70,71 @@ plot(sites_dom_hti,
 
 plot(dom_hti_line, add = TRUE)
 # there are some repeat entries, I think that sites can count as more than one thing
-# I believe the bottom of the map is getting cut off in the health site data and that's carrying over, might be one or two points missing
+# I believe the bottom of the map is getting cut off in the health site data and that's carrying over, so there might be one or two points missing
 
 
-#### DISTANCE TO COASTLINE ####
+#### RANDO CODE ####
+
+dist_test1 = terra::distance(dom_hti_line, sites_dom_hti,
+                             unit = "km")
+
+dist_test1 = as.data.frame(dist_test1_)
+dist_test1 = t(dist_test1)
+sites_dom_hti = cbind(sites_dom_hti, dist_test1)
+
+plot(sites_dom_hti,
+     "V1")
+# transpose with t?
 
 sites_sf = sf::st_as_sf(sites_dom_hti)
 line_sf = sf::st_as_sf(dom_hti_line)
 
-dist_test = st_distance(line_sf,
-                        sites_sf)
+dist_test = st_distance(line_sf, sites_sf)
 
-distance_df = dist_test %>%
-  t() %>% # transpose to set up columns vs. rows for data frame?
-  as.data.frame
+# distance_df = dist_test %>%
+  # t() %>% # transpose to set up columns vs. rows for data frame?
+  # as.data.frame
 
-units(distance_df$.) = "km"  # set to kilometers 
+# units(distance_df$.) = "km"  # set to kilometers 
 
 # full data frame with raster IDs
-sites_dom_hti$distance = distance_df[, 1]
+# sites_dom_hti$distance = distance_df[, 1]
 
-plot(sites_dom_hti, 
-     breaks = c(0, 10, 50, 100), 
-     "distance")
+# plot(sites_dom_hti, 
+    # breaks = c(0, 5, 10, 20, 50), 
+    # "distance")
 
-plot(dom_hti_line, add = TRUE)
+# plot(dom_hti_line, add = TRUE)
 # seems that some of these are getting misclassified
 # zeros are still concerning
+
+
+
+#### DAVI'S CODE ####
+
+# create blank raster and call line into raster
+dom_hti_rast = rast(ext = ext(dom_hti_line), res = 0.009, crs = crs(dom_hti_line))
+line_rast = rasterize(dom_hti_line, dom_hti_rast)
+# not sure what res is doing here
+# crs shouldn't matter but we can keep it
+
+# calculate distance
+dom_hti_dist = terra::distance(line_rast)
+# mask to original polygon
+dom_hti_dist = mask(dom_hti_dist, dom_hti)
+# masking back to polygon instead of line here -- returns a blank plot if you mask to line
+
+plot(dom_hti_dist)
+plot(dom_hti_line, add = TRUE)
+
+# create dataframe, extract distance values for points and attach to attribute
+dist_df = terra::extract(
+  x = dom_hti_dist,
+  y = sites_dom_hti,
+  na.rm = TRUE,
+)
+
+dom_hti_line$distance = dist_df[, 2]
+
+plot(dist_df, col = map.pal("elevation", 3))
+# units are meters. concerned that there are 0s
